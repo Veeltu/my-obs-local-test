2bdb67f5276ee6c9987a1154b04f5b90f6afbee5
PawełAndrys <pawelandrys@gmail.com>
2025-06-04 10:07:25 +0200

    rework fix5

--- otel-collector.tf
+++ otel-collector.tf
@@ -78,12 +78,14 @@ resource "kubernetes_deployment_v1" "otel_collector" {
     }
   }
   spec {
-    replicas = 1
     selector {
       match_labels = {
         app = "otel-collector"
       }
     }
+    min_ready_seconds         = 5
+    progress_deadline_seconds = 120
+    replicas                  = 1
     template {
       metadata {
         labels = {
@@ -101,58 +103,115 @@ resource "kubernetes_deployment_v1" "otel_collector" {
             name       = "otel-collector-config"
             mount_path = "/etc/otel"
           }
-          env {
-            name = "MY_POD_IP"
-            value_from {
-              field_ref {
-                field_path = "status.podIP"
-              }
+          # volume_mount {
+          #   name       = "secrets"
+          #   mount_path = "/etc/otelcol-contrib/secrets"
+          #   read_only  = true
+          # }
+          resources {
+            limits = {
+              memory = "2Gi"
+            }
+            requests = {
+              cpu    = "200m"
+              memory = "400Mi"
             }
           }
-          env {
-            name = "KAFKA_USERNAME"
-            value_from {
-              secret_key_ref {
-                name = "kafka-credentials"
-                key  = "username"
-              }
-            }
-          }
-          env {
-            name = "KAFKA_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = "kafka-credentials"
-                key  = "password"
-              }
-            }
-          }
-          env {
-            name = "SNMP_AUTH_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = "snmp-credentials"
-                key  = "auth-password"
-              }
-            }
-          }
-          env {
-            name = "SNMP_PRIVACY_PASSWORD"
-            value_from {
-              secret_key_ref {
-                name = "snmp-credentials"
-                key  = "privacy-password"
-              }
-            }
-          }
-          env {
-            name = "K8S_NODE_NAME"
-            value_from {
-              field_ref {
-                field_path = "spec.nodeName"
-              }
-            }
-          }
+          # env {
+          #   name = "MY_POD_IP"
+          #   value_from {
+          #     field_ref {
+          #       field_path = "status.podIP"
+          #     }
+          #   }
+          # }
+          # env {
+          #   name = "KAFKA_USERNAME"
+          #   value_from {
+          #     secret_key_ref {
+          #       name = "kafka-credentials"
+          #       key  = "username"
+          #     }
+          #   }
+          # }
+          # env {
+          #   name = "KAFKA_PASSWORD"
+          #   value_from {
+          #     secret_key_ref {
+          #       name = "kafka-credentials"
+          #       key  = "password"
+          #     }
+          #   }
+          # }
+          # env {
+          #   name = "SNMP_AUTH_PASSWORD"
+          #   value_from {
+          #     secret_key_ref {
+          #       name = "snmp-credentials"
+          #       key  = "auth-password"
+          #     }
+          #   }
+          # }
+          # env {
+          #   name = "SNMP_PRIVACY_PASSWORD"
+          #   value_from {
+          #     secret_key_ref {
+          #       name = "snmp-credentials"
+          #       key  = "privacy-password"
+          #     }
+          #   }
+          # }
+          # env {
+          #   name = "K8S_NODE_NAME"
+          #   value_from {
+          #     field_ref {
+          #       field_path = "spec.nodeName"
+          #     }
+          #   }
+          # }
+          #           port {
+          #   container_port = 54526
+          #   name           = "syslogtcp"
+          # }
+          # port {
+          #   container_port = 54527
+          #   name           = "syslogudp"
+          #   protocol       = "UDP"
+          # }
+          # port {
+          #   container_port = 54528
+          #   name           = "syslogtcptls"
+          # }
+          # # Default endpoint for ZPages.
+          # port {
+          #   container_port = 55679
+          #   name           = "zpages"
+          # }
+          # # Default endpoint for OpenTelemetry receiver.
+          # port {
+          #   container_port = 4317
+          #   name           = "otel-grpc"
+          # }
+          # # Default endpoint for Jaeger gRPC receiver.
+          # port {
+          #   container_port = 14250
+          #   name           = "jaeger-grpc"
+          # }
+          # # Default endpoint for Jaeger HTTP receiver.
+          # port {
+          #   container_port = 14268
+          #   name           = "jaeger-http"
+          # }
+          # # Default endpoint for Zipkin receiver.
+          # port {
+          #   container_port = 9411
+          #   name           = "zipkin"
+          # }
+          # # Default endpoint for querying metrics.
+          # port {
+          #   container_port = 8888
+          #   name           = "metrics"
+          # }
           port {
             container_port = 4317
             name           = "grpc"
@@ -172,7 +231,30 @@ resource "kubernetes_deployment_v1" "otel_collector" {
             name = kubernetes_config_map_v1.otel_collector_config.metadata[0].name
           }
         }
+
+        # volume {
+        #   name = "secrets"
+        #   projected {
+        #     sources {
+        #       dynamic "secret" {
+        #         for_each = local.certs
+        #         content {
+        #           name = replace(secret.value, ".", "-")
+        #           items {
+        #             key  = "tls.crt"
+        #             path = "${replace(secret.value, ".", "-")}.crt"
+        #           }
+        #           items {
+        #             key  = "tls.key"
+        #             path = "${replace(secret.value, ".", "-")}.key"
+        #           }
+        #         }
+        #       }
+        #     }
+        #   }
+        # }
       }
     }
   }
+  # depends_on = [kubernetes_manifest.certs]
 }
